69ebee94df9af63eb95f0fd6b547b5e8657f54f1
wingdust <995315823@qq.com>
2022-06-12 20:50:36 +0800

    Fixed: ex ! config with pwsh

--- custom/chadrc.lua
+++ custom/chadrc.lua
@@ -82,15 +82,47 @@ map("t","<esc>","<C-\\><C-n>",opt)
 -- https://www.reddit.com/r/neovim/comments/pc7in0/detect_os_in_lua/
 if jit.os=='Windows' then
 vim.cmd([[
-set shell=pwsh
-set shellcmdflag=-command
-set shellquote=\"
-set shellxquote=
+set shell=pwsh\ -NoLogo shellpipe=\| shellxquote=
+  set shellcmdflag=-NoLogo\ -NoProfile\ -ExecutionPolicy\ RemoteSigned\ -Command
+  set shellredir=\|\ Out-File\ -Encoding\ UTF8
+set shellredir=\|\ Out-File\ -Encoding\ UTF8
 autocmd FileType dashboard set showtabline=0 | autocmd WinLeave <buffer> set showtabline=2
 ]])
 end
 
+--set shellcmdflag= '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+--set shellxquote="{"
+--set shell=pwsh
 
+--set shellquote= \"
+--set shellquote= shellxquote=
+--set shellxquote=
+
+local function get_visual_selection()
+  local s_start = vim.fn.getpos("'<")
+  local s_end = vim.fn.getpos("'>")
+  local n_lines = math.abs(s_end[2] - s_start[2]) + 1
+  local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+  lines[1] = string.sub(lines[1], s_start[3], -1)
+  if n_lines == 1 then
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+  else
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+  end
+  return table.concat(lines, '\n')
+end
+
+vim.api.nvim_create_user_command(
+  'OpenURL', 
+  function(opts) 
+    print(opts)
+    print(get_visual_selection())
+
+    -- cmd = ("open itter.js")
+    -- os.execute(cmd)
+  end,
+  { nargs = '*' }
+  )
 
 
 
