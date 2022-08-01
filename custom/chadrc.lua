880f2f4eab3170f05a17f9cc284c490cf4130296
wingdust <995315823@qq.com>
2022-08-01 23:25:19 +0800

    find out how to override default mapping

--- custom/chadrc.lua
+++ custom/chadrc.lua
@@ -35,19 +35,56 @@ M.plugins = {
   }
 
 }
+M.disabled ={
+  i = {
+    
+      -- go to  beginning and end
+      ["<C-b>"] = "",
+                  "",
+      -- navigate 
+      ["<C-h>"] = "",
+      ["<C-l>"] = "",
+      ["<C-j>"] = "",
+      ["<C-k>"] = "",
+  }
+}
 
 M.mappings ={
 -- navigation in insert mode, only if enabled in options
- insert_nav = {
-    --backward = "<C-h>",
---    end_of_line = "<C-e>",
---    forward = "<C-f>",
---    next_line = "<C-n>",
---    prev_line = "<C-p>",
---    beginning_of_line = "<C-a>",
- },
-terminal ={
- --esc_termmode={ "<esc>"}
+--  insert_nav = {
+--     backward          = {"<C-b>","backward"},
+--     end_of_line       = {"<C-e>","end_of_line"},
+--     forward           = {"<C-f>","forward"},
+--     next_line         = {"<C-n>","next_line"},
+--     prev_line         = {"<C-p>","prev_line"},
+--     beginning_of_line = {"<C-a>","beginning_of_line"},
+--  },
+--
+-- 它这里的emacs 是一个用户定义的名字，
+  emacs ={
+    i ={
+        ["<C-a>"] = { "<ESC>^i", "home" },
+        ["<C-b>"] = { "<Left>", "back" },
+        ["<C-f>"] = { "<Left>", "forward" },
+        ["<C-p>"] = { "<Up>", "previous" },
+        ["<C-n>"] = { "<Down>", "next" },
+    }
+  },
+  terminal ={
+   --esc_termmode={ "<esc>"}
+  },
+disabled = {
+  i = {
+    
+      -- go to  beginning and end
+      ["<C-b>"] = "",
+                  "",
+      -- navigate 
+      ["<C-h>"] = "",
+      ["<C-l>"] = "",
+      ["<C-j>"] = "",
+      ["<C-k>"] = "",
+  }
 }
 
 }
@@ -88,6 +125,24 @@ if jit.os=='Windows'
     set shellcmdflag=-NoLogo\ -NoProfile\ -ExecutionPolicy\ RemoteSigned\ -Command
     set shellredir=\|\ Out-File\ -Encoding\ UTF8
   set shellredir=\|\ Out-File\ -Encoding\ UTF8
+
+nnoremap <silent> za <Cmd>call VSCodeNotify('editor.toggleFold')<CR>
+nnoremap <silent> zR <Cmd>call VSCodeNotify('editor.unfoldAll')<CR>
+nnoremap <silent> zM <Cmd>call VSCodeNotify('editor.foldAll')<CR>
+nnoremap <silent> zo <Cmd>call VSCodeNotify('editor.unfold')<CR>
+nnoremap <silent> zO <Cmd>call VSCodeNotify('editor.unfoldRecursively')<CR>
+nnoremap <silent> zc <Cmd>call VSCodeNotify('editor.fold')<CR>
+nnoremap <silent> zC <Cmd>call VSCodeNotify('editor.foldRecursively')<CR>
+
+nnoremap <silent> z1 <Cmd>call VSCodeNotify('editor.foldLevel1')<CR>
+nnoremap <silent> z2 <Cmd>call VSCodeNotify('editor.foldLevel2')<CR>
+nnoremap <silent> z3 <Cmd>call VSCodeNotify('editor.foldLevel3')<CR>
+nnoremap <silent> z4 <Cmd>call VSCodeNotify('editor.foldLevel4')<CR>
+nnoremap <silent> z5 <Cmd>call VSCodeNotify('editor.foldLevel5')<CR>
+nnoremap <silent> z6 <Cmd>call VSCodeNotify('editor.foldLevel6')<CR>
+nnoremap <silent> z7 <Cmd>call VSCodeNotify('editor.foldLevel7')<CR>
+
+xnoremap <silent> zV <Cmd>call VSCodeNotify('editor.foldAllExcept')<CR>
 
 
   autocmd FileType dashboard set showtabline=0 | autocmd WinLeave <buffer> set showtabline=2
